9ebdf81326f3bebea3d3bccb20c6ab6df7e300ee
carlosandf <carlos.andres.ff2912@gmail.com>
2023-08-23 18:59:18 -0500

    Use form data for search movies

---
+++ src/services/search_form/search_form.js
@@ -0,0 +1,10 @@
+import { setLocationHash } from '../../utils/set_location_hash.js';
+
+export function searchMovies (form) {
+  const formData = new FormData(form);
+
+  const data = Object.fromEntries(formData);
+
+  if (!data || !Object.keys(data).length > 0) return;
+  setLocationHash(`search=${data.search_input}`);
+}

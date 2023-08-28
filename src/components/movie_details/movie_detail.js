c0829c7d03881e7b6e36d74a3635b6e6a9dc6feb
carlosandf <carlos.andres.ff2912@gmail.com>
2023-08-27 22:10:24 -0500

    Add categories container & update styles

--- src/components/movie_details/movie_detail.js
+++ src/components/movie_details/movie_detail.js
@@ -1,3 +1,6 @@
+import { carousel } from '../carousel/carousel.js';
+import { Category } from '../category/category.js';
+import { Poster } from '../poster/poster.js';
 import styles from './movie_detail.module.css';
 
 const $ = document;
@@ -17,9 +20,16 @@ voteAverage.className = styles['vote-average'];
 const overviewText = $.createElement('p');
 overviewText.className = styles['overview-text'];
 
+const categoriesContainer = $.createElement('section');
+categoriesContainer.className = styles['categories-container'];
+
+const relatedMoviesContainer = $.querySelector('#related-movies');
+relatedMoviesContainer.classList.add(styles['related-movies-container'], 'inactive');
+
 info.append(
   voteAverage,
-  overviewText
+  overviewText,
+  categoriesContainer
 );
 
 infoContainer.append(
@@ -27,8 +37,31 @@ infoContainer.append(
   info
 );
 
-export function movieInfo ({ title, overview, vote_average }) {
+export function movieInfo ({ title, overview, vote_average, genres }) {
   movieTitle.textContent = title;
-  voteAverage.textContent = `${vote_average} / 10`;
+
+  const average = Number(vote_average);
+  voteAverage.textContent = `${average.toFixed(1)} / 10`;
+
   overviewText.textContent = overview;
+
+  categoriesContainer.textContent = '';
+  genres.forEach(genre => categoriesContainer.appendChild(Category(genre)));
+
+  const relatedMovies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  if (relatedMovies.length > 0) {
+    const carouselContainer = carousel();
+
+    if (relatedMoviesContainer.children[1]) {
+      relatedMoviesContainer.removeChild(relatedMoviesContainer.children[1]);
+    }
+    relatedMoviesContainer.classList.remove('inactive');
+    relatedMoviesContainer.append(carouselContainer);
+
+    relatedMovies?.forEach(movie => {
+      carouselContainer.appendChild(
+        Poster({ movie })
+      );
+    });
+  }
 }

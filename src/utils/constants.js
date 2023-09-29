67ae470c3e260b1b0ec7750100d19882bfbe8837
carlosandf <carlos.andres.ff2912@gmail.com>
2023-09-29 12:22:31 -0500

    Use image compress

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
+import { compressImage } from './compress_image';
+
 export const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 export const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
 export const BASE_URL = 'https://api.themoviedb.org/3';
@@ -6,9 +8,12 @@ export const IMAGE_URL = 'https://image.tmdb.org/t/p';
 export const getImage = ({ size, path }) => {
   if (size) return `${IMAGE_URL}/w${size}${path}`;
 
-  if (window.outerWidth <= 550) {
-    return `${IMAGE_URL}/w400${path}`;
-  } else {
-    return `${IMAGE_URL}/original${path}`;
-  }
+  return new Promise((resolve, reject) => {
+    if (window.outerWidth <= 550) {
+      return resolve(`${IMAGE_URL}/w400${path}`);
+    } else {
+      compressImage(`${IMAGE_URL}/original${path}`, 20)
+        .then(({ urlCompres }) => resolve(urlCompres));
+    }
+  });
 };

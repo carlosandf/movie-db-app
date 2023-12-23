19fb50ca746969aa6ed0b67f0205e8a672d2f52d
carlosandf <carlos.andres.ff2912@gmail.com>
2023-12-23 18:21:35 -0500

    Remove defaul load image from constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,6 @@ export const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 export const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
 export const BASE_URL = 'https://api.themoviedb.org/3';
 export const IMAGE_URL = 'https://image.tmdb.org/t/p';
-export const defaultLoadImage = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=';
 
 export const getImage = ({ size, path }) => {
   if (size) return `${IMAGE_URL}/w${size}${path}`;
